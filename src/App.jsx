ccc37c83577d81c20ceccfbf8c4a6355f2a41ce8
CherokiDev <miguelangelmorato84@gmail.com>
2022-12-16 18:24:30 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const App = () => {
+  const [categories, setCategories] = useState([""]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+
+    setCategories([newCategory, ...categories]);
+    // setCategories((cat) => [newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>Buscador de Gifs</h1>
+
+      <AddCategory onNewCategory={(value) => onAddCategory(value)} />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+export const getGifs = async (category) => {
+  const url = `https://​api.giphy.com/v1/gifs/search?api_key=CzKGDDg4fDaWNHZ8H4B1SHjc5jbPcsmH&q=${category}&limit=10`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+
+  return gifs;
+};
